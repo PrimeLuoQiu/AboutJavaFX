5dda5307fcbd578ecb03de7f612d25e7461c1c8e
wjh <431756152@qq.com>
2024-06-18 20:37:03 +0800

    add lesson 7

---
+++ Lessons/Lesson7/Main.java
@@ -0,0 +1,21 @@
+package Lessons.Lesson7;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
